627398245ccbc0ecc70d61fcc24a616142a5b97d
ogarcia-tech <ogarcia@metricaweb.es>
2025-07-19 19:52:57 +0200

    Fix conversation saved hook

--- ai-chatbot-pro/includes/class-ajax-handler.php
+++ ai-chatbot-pro/includes/class-ajax-handler.php
@@ -51,8 +51,8 @@ class AICP_Ajax_Handler {
             $format[] = '%s';
         }
 
-        if($log_id > 0) { $wpdb->update($table_name, $data, ['id' => $log_id], $format, ['%d']); } 
-        else { $wpdb->insert($table_name, $data, $format); $log_id = $wpdb->insert_id; do_action('aicp_conversation_saved', $log_id, $assistant_id, $conversation_log);}
+        if($log_id > 0) { $wpdb->update($table_name, $data, ['id' => $log_id], $format, ['%d']); }
+        else { $wpdb->insert($table_name, $data, $format); $log_id = $wpdb->insert_id; do_action('aicp_conversation_saved', $log_id, $assistant_id, $conversation);} 
 
         return $log_id;
     }
